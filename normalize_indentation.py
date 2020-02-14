2f957c5b761e0ecb6d341e6adb252a75ba8f1e92
EC <eugene.chernikov@yatta.works>
2020-02-14 15:16:56 +0300

    Update for modern ST3
    — FIXED — modern ST3 syntax def. files are 'sublime-syntax’, not ‘tmLanguage’
    — add better indentation detection via https://github.com/jfcherng/Sublime-AutoSetIndentation
    — clean up code a bit

--- normalize_indentation.py
+++ normalize_indentation.py
@@ -2,51 +2,135 @@ import sublime, sublime_plugin, time
 
 class NormalizeIndentationCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        print('Starting to normalize indentation')
-        global_settings = sublime.load_settings('Preferences.sublime-settings') #First get global settings
-        syntax = self.view.settings().get('syntax') #next we need the syntax settings
-        segments = syntax.split('/')
-        language = ''
-        for segment in segments:
-            if (segment.endswith('.tmLanguage')):
-                language = segment.replace('.tmLanguage', '')
-        syntax_settings = sublime.load_settings(language+'.sublime-settings')
-        wanted_tabsize = global_settings.get('tab_size') #use the global settings later we overwrite it with syntax settings if there are any
-        translate_tabs_to_spaces = global_settings.get('translate_tabs_to_spaces');
-        if(syntax_settings):
-            if(syntax_settings.get('tab_size')):
-                wanted_tabsize = syntax_settings.get('tab_size')
-            if(syntax_settings.get('translate_tabs_to_spaces')):
-                translate_tabs_to_spaces = syntax_settings.get('translate_tabs_to_spaces')
-        self.view.run_command('detect_indentation') #get the current indentation state
-        view_tabsize = self.view.settings().get('tab_size')
-        view_translate_tabs_to_spaces = self.view.settings().get('translate_tabs_to_spaces')
-        if (translate_tabs_to_spaces == False): #we want tabs and don't care what the current state is
-            print('converting spaces to tabs')
-            self.view.run_command('unexpand_tabs')
-            self.view.settings().set('tab_size', wanted_tabsize)
-        elif (view_translate_tabs_to_spaces == False and translate_tabs_to_spaces == True and view_tabsize == wanted_tabsize): #we have tabs but want spaces
-            print('converting tabs to spaces')
-            self.view.run_command('expand_tabs')
-            self.view.settings().set('translate_tabs_to_spaces', True)
-        elif (view_translate_tabs_to_spaces == True and view_tabsize != wanted_tabsize): #we have spaces but the tab_size does not fit
-            time.sleep(1)
-            print('setting right tabsize')
-            self.view.run_command('unexpand_tabs') #first convert to tabs
-            self.view.settings().set('tab_size', wanted_tabsize) # change size
-            self.view.run_command('expand_tabs') #expand again
-            self.view.settings().set('translate_tabs_to_spaces', True)
-        else:
-            print('all is fine and dandy')
-        print('Done normalizing indentation')
+        self.log('\n\n\n', '== * == * == * == * ==', 'NormaliseIndentation:')
+        self.prefs_base = self.get_prefs_base()
+        self.prefs_view = self.get_prefs_view()
 
-        sublime.status_message('Done normalizing indentation')
+        have_spaces, want_spaces, same_tab_size = [
+            self.prefs_view['translate_tabs_to_spaces'],
+            self.prefs_base['translate_tabs_to_spaces'],
+            self.prefs_view['tab_size'] == self.prefs_base['tab_size']
+        ]
+
+        if have_spaces:
+            if want_spaces:
+                if same_tab_size:
+                    # nothing to do, but in case there are leftover \t's...
+                    self.log("action taken", {
+                        'from': 'spaces',
+                        'to': 'spaces',
+                        'size': 'same',
+                        'do':'replace tabs with spaces just in case'
+                        })
+                    self.expand_tabs_to_spaces()
+                else:
+                    # normalize tab width by substition round-trip
+                    self.log("action taken", {
+                        'from': 'spaces',
+                        'to': 'spaces',
+                        'size': 'MISMATCH',
+                        'do':'replacement roundtrip!'
+                    })
+                    self.collapse_spaces_to_tabs()
+                    self.set_tab_size_to_default()
+                    self.expand_tabs_to_spaces()
+            else: # want_tabs
+                # whatever are tab widths we can just collapse
+                self.log("action taken", {
+                    'from': 'spaces',
+                    'to': 'tabs',
+                    'size': 'whatever',
+                    'do': 'collapse spaces seq`s to tabs'
+                    })
+                self.collapse_spaces_to_tabs()
+                self.set_tab_size_to_default()
+        else: # have tabs
+            self.log("action taken", {
+                'from': 'tabs',
+                'to': 'whatever',
+                'size': 'whatever',
+                'do':'expand tabs to spaces'
+                })
+            self.set_tab_size_to_default() # won't hurt anyway
+            if want_spaces:
+                # now it can get SPACE'd if requred
+                self.expand_tabs_to_spaces()
+
+    def extract_indentation_prefs(self, st_settings):
+        result = {}
+        for n in ['tab_size', 'translate_tabs_to_spaces']:
+            # we need False values too
+            if st_settings.get(n) != None: 
+                result[n] = st_settings.get(n)
+        return result
+
+    def get_prefs_base(self):
+        """combine editor defaults and language-specific settings"""
+        prefs_base = {}
+
+        settings_subl = sublime.load_settings('Preferences.sublime-settings')
+        prefs_subl = self.extract_indentation_prefs(settings_subl)
+        self.log("-- indentation prefs for editor", prefs_subl)
+        prefs_base.update(prefs_subl)
+
+        syntax = self.view.settings().get('syntax').split('/')[-1]
+        language = syntax.replace('.tmLanguage', '').replace('.sublime-syntax', '')
+        settings_lang = sublime.load_settings(language + '.sublime-settings')
+        prefs_lang = self.extract_indentation_prefs(settings_lang)
+        self.log("-- indentation prefs (syntax) for " + syntax, prefs_lang)
+        prefs_base.update(prefs_lang)
+        
+        self.log("-- indentation prefs (merged local):", prefs_base)
+        return prefs_base
+
+    def get_prefs_view(self):
+        """get indentation settings for active tab"""
+
+        # The default implementation of detect_lines requires 10 indented lines before it makes a guess
+        # on indentation at all, and often make mistakes.
+        # there's a better indent. detector: https://github.com/jfcherng/Sublime-AutoSetIndentation
+        # which hooks into default command,
+        self.view.run_command('detect_indentation')
+
+        freshly_updated_view_settings = self.view.settings()
+        prefs_view = self.extract_indentation_prefs(freshly_updated_view_settings)
+        self.log("-- prefs_view:", prefs_view)
+        return prefs_view
+
+    ## whitespace manipulation methods
+    def collapse_spaces_to_tabs(self):
+        self.clean_up_operations_queue()
+        self.view.run_command('unexpand_tabs')
+        self.view.settings().set('translate_tabs_to_spaces', False)
+
+    def expand_tabs_to_spaces(self):
+        self.clean_up_operations_queue()
+        self.view.run_command('expand_tabs')
+        self.view.settings().set('translate_tabs_to_spaces', True)
+
+    def set_tab_size_to_default(self):
+        self.clean_up_operations_queue()
+        self.view.settings().set('tab_size', self.prefs_base['tab_size'])
+
+    def clean_up_operations_queue(self):
+        # e.g., sometimes self.view == None, we used time.sleep(1) before
+        while not self.view:
+            pass
+
+    def log(self, *payload):
+        if sublime.load_settings('Normalize Indentation.sublime-settings').get('debug_mode', False):
+            for chunk in payload:
+                print(chunk)
+
 class NormalizeIndentationOnOpen(sublime_plugin.EventListener):
+
     def on_load(self, view):
         settings = sublime.load_settings('Normalize Indentation.sublime-settings')
-        if(settings.get('convert_on_open', True)): #only run if the user wants it, default value is False
+        if settings.get('convert_on_open', False): 
             view.run_command('normalize_indentation')
-    def on_activated(self, view): #run when view gains editing focus
+
+    # view gains editing focus
+    def on_activated(self, view): 
         settings = sublime.load_settings('Normalize Indentation.sublime-settings')
-        if(settings.get('convert_on_activate', True)):
+        if settings.get('convert_on_activate', False):
             view.run_command('normalize_indentation')
