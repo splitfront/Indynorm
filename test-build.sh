cc8e3ee6a51ddca1caeeff9cad2b55e818629eb9
EC <ujin@vomit-komet.local>
2020-02-18 17:12:23 +0300

    Quick’n’dirty way to test plugin without pushig to github

---
+++ test-build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# KISS shellscript to quickly test plugin without ruindtrip to github first
+# (to run properly, it has to live in ${packages} sans package-metadata.json --
+# lest ST3 rightfully kill the whole folder as a potential orphaned package)
+
+rm -rf ~/temp/Normalise_Indentation_with_extra_crap
+mkdir -p ~/temp/Normalise_Indentation_with_extra_crap
+cp -r * ~/temp/Normalise_Indentation_with_extra_crap
+rm -r ~/temp/Normalise_Indentation_with_extra_crap/samples
+rm ~/temp/Normalise_Indentation_with_extra_crap/test-build.sh
+rm ~/temp/Normalise_Indentation_with_extra_crap/package-metadata.json
+rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Normalise-Indentation
+sleep 1s
+mv ~/temp/Normalise_Indentation_with_extra_crap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Normalise-Indentation
+
+echo "Upped installed version."
