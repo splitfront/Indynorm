cc5981e322ef025e0ff8bb05efdda6e14e4bd620
EC <ujin@vomit-komet.local>
2020-02-22 21:53:10 +0300

    Add info on working with buffers and multiple views

--- snippets.py
+++ snippets.py
@@ -5,3 +5,9 @@ view.substr(sublime.Region(0, view.size()))
 # get current cursor position
 
 (row,col) = self.view.rowcol(self.view.sel()[0].begin())
+
+# overwrite view content (even for unsaved views)
+# https://forum.sublimetext.com/t/how-to-overwrite-the-content-of-an-untitled-view/32814/6
+
+# views vs buffers
+# https://forum.sublimetext.com/t/what-is-a-buffer/18353/2
