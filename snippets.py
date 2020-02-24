131e0bdbb8e46592300a3d34832cadbb7e60704e
EC <ujin@vomit-komet.local>
2020-02-24 17:04:45 +0300

    Comment out unwanted snippets of just-in-case code

--- snippets.py
+++ snippets.py
@@ -1,13 +1,13 @@
-# get buffer contents
+## get buffer contents
 
-view.substr(sublime.Region(0, view.size()))
+# view.substr(sublime.Region(0, view.size()))
 
-# get current cursor position
+## get current cursor position
 
-(row,col) = self.view.rowcol(self.view.sel()[0].begin())
+# (row,col) = self.view.rowcol(self.view.sel()[0].begin())
 
-# overwrite view content (even for unsaved views)
+## overwrite view content (even for unsaved views)
 # https://forum.sublimetext.com/t/how-to-overwrite-the-content-of-an-untitled-view/32814/6
 
-# views vs buffers
+## views vs buffers
 # https://forum.sublimetext.com/t/what-is-a-buffer/18353/2
