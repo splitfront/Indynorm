23e39de5b43e19af8f0dc4e6a4b2d5c8076d8b10
EC <ujin@vomit-komet.local>
2020-02-25 19:13:29 +0300

    ADD a sample of js with weird indentation

---
+++ samples/indentation_jump.js
@@ -0,0 +1,11 @@
+// after 4-to-8 tabs indentation conversion left margin of lines with
+// &&s should stay 12 spaces wide
+
+if (foo 
+// <- 12  ->
+            && bar
+            && quux || zot ) {
+    bring_it_on();
+} else {
+    slack_off();
+}
